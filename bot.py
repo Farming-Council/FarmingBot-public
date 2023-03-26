739abb1e5ad49a1d9abce058763149c875513fae
Dyslexus <robertcirlanm@gmail.com>
2023-03-26 20:14:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,4 +10,4 @@ load_dotenv()
 bot = FarmingCouncil()
 
 if __name__ == "__main__":
-    bot.run("MTA1NzE2ODU3OTcxMzMxMDgyMQ.GXgQ7N.do1zwgAUZq3QmLyO8uQHepetNl7k7ObYpJx96s")
+    bot.run(os.environ.get("TOKEN"))
